d3b6765fd32ce1359b1db89ff4674eb0a4f7a5b8
Pawel Kurzawa <pkurzawa90@gmail.com>
2017-11-22 13:23:55 +0100

    Add index.html and scripts.js + Change content of .js file

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var a = 10;
+var h = 15;
+var triangleArea = a*h/2;
+
+console.log('Traingle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+prompt('Traingle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); 
