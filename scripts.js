92160a92adadc733008b34329030ba401f49187c
Pawel Kurzawa <pkurzawa90@gmail.com>
2017-11-22 20:24:11 +0100

    Change in .js file

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-var a = 10;
-var h = 15;
+var a = prompt('Podaj a');
+var h = prompt('Podaj h');
 var triangleArea = a*h/2;
 
 console.log('Traingle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
-prompt('Traingle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); 
+alert('Traingle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); 
